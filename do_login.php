9f7e3c187e02149fd6343643973de6c70a214e03
AshayC <ashaychangwani@gmail.com>
2018-10-17 22:32:11 +0530

    First

---
+++ do_login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if(isset($_POST['do_login']))
+{
+ $host="localhost";
+ $username="root";
+ $password="";
+ $databasename="sample";
+ $connect=mysql_connect($host,$username,$password);
+ $db=mysql_select_db($databasename);
+
+ $email=$_POST['email'];
+ $pass=$_POST['password'];
+ $select_data=mysql_query("select * from user where email='$email' and password='$pass'");
+ if($row=mysql_fetch_array($select_data))
+ {
+  $_SESSION['email']=$row['email'];
+  echo "success";
+ }
+ else
+ {
+  echo "fail";
+ }
+ exit();
+}
+?>
